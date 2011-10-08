7fecb0cddecc0905c43da2534490ea785aee9406
Bryce Sheehan <bryce.sheehan@gmail.com>
2011-10-08 20:14:56 +0800

    Initial Commit

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+xhr=new XMLHttpRequest();
+xhr.open('GET','https://gdata.youtube.com/feeds/api/videos/'+yt.config_.VIDEO_ID+'?v=2&alt=json',false);
+xhr.send();
+data = eval('('+xhr.responseText+')');
+yt.www.watch.player.seekTo(Math.floor(data.entry.media$group.yt$duration.seconds/3));

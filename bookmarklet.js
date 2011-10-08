1b2db1c5a347015cceb4fd506d86de0cd58050b9
Bryce Sheehan <bryce.sheehan@gmail.com>
2011-10-08 20:40:32 +0800

    Fixed Wadsworth Constant value

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,4 +2,4 @@ xhr=new XMLHttpRequest();
 xhr.open('GET','https://gdata.youtube.com/feeds/api/videos/'+yt.config_.VIDEO_ID+'?v=2&alt=json',false);
 xhr.send();
 data = eval('('+xhr.responseText+')');
-yt.www.watch.player.seekTo(Math.floor(data.entry.media$group.yt$duration.seconds/3));
+yt.www.watch.player.seekTo(Math.floor(data.entry.media$group.yt$duration.seconds*0.3));
